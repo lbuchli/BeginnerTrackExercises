6838b7da4d1371a7575e5b4cf3162e8847d323c0
Lukas Buchli <lbu01@buchli-bigger.ch>
2023-06-09 21:27:40 +0200

    Add solutions for chapter 07

--- Ch07/Exercises.hs
+++ Ch07/Exercises.hs
@@ -1,21 +1,28 @@
 -- Chapter 07: Modules
-module Exercises where
+module Exercises (wordCount, keySet, union1, union2) where
+
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Data.Set (Set)
+import qualified Data.Set as Set
+
+import Data.List (delete, intersect, nub)
 
 {-|
 Exercise: Using the functions you just learned about, implement a function that
 counts the number of words in a text.
 |-}
 
-wordCount :: _
-wordCount = _
+wordCount :: String -> Int
+wordCount = length . words
 
 {-|
 Exercise: Using Data.Map and Data.Set, implement a function that converts a Map
 to a Set of keys.
 |-}
 
-keySet :: _
-keySet = _
+keySet :: Ord k => Map k v -> Set k
+keySet = Set.fromList . Map.keys
 
 {-|
 Exercise: Implement 'union' for lists using
@@ -23,11 +30,11 @@ Exercise: Implement 'union' for lists using
 (2) Only 'filter', 'foldr', 'intersect' and (++)
 |-}
 
-union1 :: _
-union1 = _
+union1 :: Eq a => [a] -> [a] -> [a]
+union1 = foldr (\z zs -> z : delete z zs)
 
-union2 :: _
-union2 = _
+union2 :: Eq a => [a] -> [a] -> [a]
+union2 xs ys = xs ++ filter (`notElem` intersect xs ys) (nub ys)
 
 {-|
 Exercise: Export all functions you have defined in this module explicitly.
