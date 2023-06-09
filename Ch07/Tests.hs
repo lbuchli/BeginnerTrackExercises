357a72ed275259b815fa6b9b1ee2691b29c05228
Lukas Buchli <lbu01@buchli-bigger.ch>
2023-06-09 21:28:08 +0200

    Add tests for chapter 07

---
+++ Ch07/Tests.hs
@@ -0,0 +1,30 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
+import Test.Hspec
+import Test.QuickCheck
+
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Data.Set (Set)
+import qualified Data.Set as Set
+
+import Data.List (union, sort)
+
+import Exercises
+
+main :: IO ()
+main = hspec $ do
+  describe "wordCount" $
+    it "behaves like 'length . words'" $ property $
+        \s -> wordCount s == (length . words) s
+  describe "keySet" $
+    it "behaves like 'Set.fromList . Map.keys'" $ property $
+        \(m :: Map Int String) -> keySet m == (Set.fromList . Map.keys) m
+  describe "union1" $
+    it "behaves like 'union'" $ property $
+        -- we don't care about order
+        \(xs :: [Bool]) (ys :: [Bool]) -> sort (union1 xs ys) == sort (union xs ys)
+  describe "union2" $
+    it "behaves like 'union'" $ property $
+        -- we don't care about order
+        \(xs :: [Bool]) (ys :: [Bool]) -> sort (union2 xs ys) == sort (union xs ys)
