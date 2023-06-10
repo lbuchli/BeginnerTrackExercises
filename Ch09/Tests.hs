3cb43b9cbfc374e7ca3ef4bfb0760ae202df9f6e
Lukas Buchli <lbu01@buchli-bigger.ch>
2023-06-10 10:48:24 +0200

    Fix tests for chapter 09

--- Ch09/Tests.hs
+++ Ch09/Tests.hs
@@ -3,12 +3,14 @@
 import Test.Hspec
 import Test.QuickCheck
 
+import System.Random (mkStdGen)
+
 import Exercises
 
 main :: IO ()
 main = hspec $ do
-  describe "probablyCommutative" $
+  describe "probablyCommutative" $ do
     it "returns True on (+)" $ property $
-        \g -> probablyCommutative g (+)
+        \i -> probablyCommutative (mkStdGen i) (+)
     it "returns False on (-)" $ property $
-        \g -> probablyCommutative g (-)
+        \i -> not $ probablyCommutative (mkStdGen i) (-)
